2d7a8313be36c2d5fd209fd27808e242a5812741
prakarshsaxena <prakarshsaxena31@gmail.com>
2018-04-05 13:55:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    struct Student
+    {
+        int a;
+        char b;
+        char c;
+        float f;
+    }s1;
+        float k;
+    printf("%d \n",sizeof(k));
+    printf("%d \n",sizeof(s1));
+    return 0;
+}
